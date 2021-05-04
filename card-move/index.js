a586452942e0c20f2bc0f4ab72dd291c0dce956f
Devesh Vasnani <dvasnani@my.centennialcollege.ca>
2021-05-04 19:34:30 -0400

    Runs async function

--- card-move/index.js
+++ card-move/index.js
@@ -1,26 +1,30 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
 
-try {
-
-  // `who-to-greet` input defined in action metadata file
-  const to = core.getInput('TO_COLUMN_ID');
-  const token = core.getInput('REPOSITORY_TOKEN');
-  const cardId = core.getInput('CARD_ID');
-  console.log(cardId, to, token);
-
-  const octokit = github.getOctokit(token);
-  const result = await octokit.projects.moveCard({
-      card_id: cardId,
-      position: 'top',
-      column_id: to
-  });
-
-  core.setOutput('response', result);
-  // Get the JSON webhook payload for the event that triggered the workflow
-  const payload = JSON.stringify(github.context.payload, undefined, 2)
-  console.log(`The event payload: ${payload}`);
-  console.log(`The github context: ${github.context}`);
-} catch (error) {
-  core.setFailed(error.message);
+async function run() {
+    try {
+    
+      // `who-to-greet` input defined in action metadata file
+      const to = core.getInput('TO_COLUMN_ID');
+      const token = core.getInput('REPOSITORY_TOKEN');
+      const cardId = core.getInput('CARD_ID');
+      console.log(cardId, to, token);
+    
+      const octokit = github.getOctokit(token);
+      const result = await octokit.projects.moveCard({
+          card_id: cardId,
+          position: 'top',
+          column_id: to
+      });
+    
+      core.setOutput('response', result);
+      // Get the JSON webhook payload for the event that triggered the workflow
+      const payload = JSON.stringify(github.context.payload, undefined, 2)
+      console.log(`The event payload: ${payload}`);
+      console.log(`The github context: ${github.context}`);
+    } catch (error) {
+      core.setFailed(error.message);
+    }
 }
+
+run()
